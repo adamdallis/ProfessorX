066207e2a94543da21323f2bc8fdf30173fb0066
jameshmread <j.h.m.read@dundee.ac.uk>
2017-08-04 10:25:09 +0100

    linted

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { FileHandler } from "./FileHandler/FileHandler";
 import { CodeInspector } from "./CodeInspector/CodeInspector";
 import { SourceCodeHandler } from "./SourceCodeHandler/SourceCodeHandler";
 
-let obj = new FileHandler();
+const obj = new FileHandler();
 obj.readFile("./src/sample.ts");
-let sourceObj = new SourceCodeHandler(obj.getSourceObject());
-let mf = new CodeInspector(obj.getSourceObject());
+const sourceObj = new SourceCodeHandler(obj.getSourceObject());
+const mf = new CodeInspector(obj.getSourceObject());
 const list = mf.findObjectsOfSyntaxKind(ts.SyntaxKind.PlusToken);
-const a =list[0];
+const a = list[0];
 sourceObj.modifyCode(a.pos, a.end, "-");
 // console.log(ts.SyntaxKind.PlusToken);
 // console.log(obj.getSourceObject().statements)
