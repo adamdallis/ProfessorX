29d340f1f042fde2b062e787e0b0fdc8b6f2b4b8
jameshmread <jamesread117@yahoo.co.uk>
2017-08-23 22:47:44 +0100

    code inspector can now retrieve function names from source file

--- src/CodeInspector/CodeInspector.spec.ts
+++ src/CodeInspector/CodeInspector.spec.ts
@@ -4,9 +4,12 @@ import { CodeInspector } from "./CodeInspector";
 
 describe("Testing CodeInspector", () => {
     const code = `
+    public testName () {
         let x: number = 3 + 9;
         const y: number = 11;
         const z = x+y;
+    }
+    public functionTwo () {}
     `;
     const sourceObj = ts.createSourceFile("", code, ts.ScriptTarget.ES5, true);
     const ci: CodeInspector = new CodeInspector(sourceObj);
@@ -22,6 +25,11 @@ describe("Testing CodeInspector", () => {
     it("All binary expressions are detected", () => {
         const actual = ci.findObjectsOfSyntaxKind(ts.SyntaxKind.BinaryExpression);
         expect(actual.length).to.equal(2);
+    });
+
+    it("All function names are found", () => {
+        const names = ci.findFunctionNames();
+        expect(names.length).to.equal(2);
     });
 
 
--- src/CodeInspector/CodeInspector.ts
+++ src/CodeInspector/CodeInspector.ts
@@ -10,6 +10,18 @@ export class CodeInspector {
         return this.retrievedObjects;
     }
 
+    public findFunctionNames () {
+        const functionNames: Array<string> = [];
+        const functionGrab = this.sourceObject
+        .getChildren()[0]
+        .getSourceFile()
+        .text.match(/(?:public)\s+(\w+)\s*(?:\()/g);
+        functionGrab.forEach((name) => {
+            functionNames.push(name.match(/\s(\w+)\s/g).toString().trim());
+        });
+        return functionNames;
+    }
+
     private findTokenObjectsOfKind (object: ts.Node, kind: ts.SyntaxKind): Array<ts.Node> {
         if (object.kind === kind) {
             this.retrievedObjects.push(object);
